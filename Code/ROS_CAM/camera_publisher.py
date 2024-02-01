ca04d9c274b4c571f1121dbf560068408f618871
Oscar Ivan Rangel Reyes <143972593+OscarivRR@users.noreply.github.com>
2024-01-31 22:38:23 -0600

    Update camera_publisher.py

--- Code/ROS_CAM/camera_publisher.py
+++ Code/ROS_CAM/camera_publisher.py
@@ -2,7 +2,7 @@
 # Con esta linea se le dice a ROS que este es un archivo fuente de Python
 
 import rospy # Rospy nos permite usar ROS con Python
-from sensor_msgs.msg import Image # Se importa Image a que se mandaran msj en forma de imagenes
+from sensor_msgs.msg import Image # Se importa Image ya que, se mandaran msj en forma de imagenes
 from cv_bridge import CvBridge # cv_bridge es un paquete que convierte imagenes de OpenCV a un msj de imagen de ROS, y vicevers (OpenCV-ROS)
 import cv2 # Aqui se importa OpenCV 
 
